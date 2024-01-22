2a594d9258ff4c998c53f46ca2c49c798af12dee
yenorii <144069387+Yenorii@users.noreply.github.com>
2024-01-21 20:37:05 -0500

    reducing map otions

--- logic.js
+++ logic.js
@@ -22,14 +22,8 @@ fetch('./data/mapdata.json')
 function updateMap() {
     var selectedYear = document.getElementById('yearFilter').value;
     var selectedRegion = document.getElementById('regionFilter').value;
-    var selectedRoadType = document.getElementById('RoadTypeFilter').value;
     var selectedPopulation = document.getElementById('PopulationFilter').value;
-    var selectedAccidentType = document.getElementById('AccidentTypeFilter').value;
-    var selectedIntersectionType = document.getElementById('IntersectionTypeFilter').value;
     var selectedLocation = document.getElementById('LocationFilter').value;
-    var selectedLighting = document.getElementById('LightingFilter').value;
-    var selectedWeatherType = document.getElementById('WeatherTypeFilter').value;
-    var selectedTime = document.getElementById('TimeFilter').value;
 
     markers.clearLayers();
     fetch('./data/mapdata.json')
@@ -51,32 +45,14 @@ function checkFilters(coord, selectedYear, selectedRegion, selectedRoadType, sel
     if (selectedYear !== 'all') {
         filterCriteria.push(parseInt(coord.YEAR) === parseInt(selectedYear));
     }
-    if (selectedRoadType !== 'all') {
-        filterCriteria.push(parseInt(coord.ROADTYPE) === parseInt(selectedRoadType));
-    }
     if (selectedPopulation !== 'all') {
         filterCriteria.push(coord.POPULATION === selectedPopulation);
-    }
-    if (selectedAccidentType !== 'all') {
-        filterCriteria.push(coord.ACCIDENTTYPE === selectedAccidentType);
-    }
-    if (selectedIntersectionType !== 'all') {
-        filterCriteria.push(coord.INTERSECTIONTYPE === selectedIntersectionType);
     }
     if (selectedLocation !== 'all') {
         filterCriteria.push(coord.LOCATION === selectedLocation);
     }
-    if (selectedLighting !== 'all') {
-        filterCriteria.push(coord.LIGHTING === selectedLighting);
-    }
     if (selectedRegion !== 'all') {
         filterCriteria.push(coord.REGION === selectedRegion);
-    }
-    if (selectedWeatherType !== 'all') {
-        filterCriteria.push(coord.WEATHERTYPE === selectedWeatherType);
-    }
-    if (selectedTime !== 'all') {
-        filterCriteria.push(coord.TIME === selectedTime);
     }
     if (filterCriteria.length > 0) {
         return filterCriteria.every(condition => condition);
@@ -90,11 +66,5 @@ updateMap();
 // Event listeners for filters
 document.getElementById('yearFilter').addEventListener('change', updateMap);
 document.getElementById('regionFilter').addEventListener('change', updateMap);
-document.getElementById('RoadTypeFilter').addEventListener('change', updateMap);
 document.getElementById('PopulationFilter').addEventListener('change', updateMap);
-document.getElementById('AccidentTypeFilter').addEventListener('change', updateMap);
-document.getElementById('IntersectionTypeFilter').addEventListener('change', updateMap);
 document.getElementById('LocationFilter').addEventListener('change', updateMap);
-document.getElementById('LightingFilter').addEventListener('change', updateMap);
-document.getElementById('WeatherTypeFilter').addEventListener('change', updateMap);
-document.getElementById('TimeFilter').addEventListener('change', updateMap);
